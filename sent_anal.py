79f20cb1e92e4a9eee9541d5af21ce76468ecd2f
afend <adfendler@gmail.com>
2019-03-31 23:42:56 -0400

    Create sent_anal.py

---
+++ sent_anal.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3.5
+import nltk
+
+'''
+Author: afend
+Version: 1.0
+'''
+
+def get_sent(story):
+    '''
+    Using the NLTK library from Python lets see if we can't train a classifier into doing the heavy lifting for us.
+    Train it positive, neutral, and negative phrases so we can get a wider range of analysis.
+
+    Input: A story
+    Output: % postive, % neutral, % negative
+    '''
+    # To do
+
+def main():
+    # To do
+
+if __name__ == "__main__":
+    main()

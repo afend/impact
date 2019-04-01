91925f7e634ced07af5c1588e16ca69660ee880d
afend <adfendler@gmail.com>
2019-03-31 23:26:16 -0400

    Update term_freq.py

--- term_freq.py
+++ term_freq.py
@@ -2,7 +2,7 @@
 
 '''
 Author: afend
-Version: 1.0
+Version: 1.1
 '''
 
 from collections import Counter
@@ -12,23 +12,17 @@ import argparse, string
 def get_story(story):
     body = []
     dir = "corpus/"
+
     with open(dir+story, "r") as f:
-        for line in f:
-            line = line.strip()
-            # Removes any empty lines
-            if line:
-                # Removes all punctuation
-                s = str.maketrans("", "", string.punctuation)
-                line = line.translate(s)
-                # Adds the entire line to the list
-                body.append(line)
+        for line  in f:
+            if line.strip():
+                body.append(line.translate(str.maketrans("", "", string.punctuation)))
+
     return body
 
 def get_tf(story):
-    body = " ".join(story).lower()
-    token = body.split(" ")
-    frequency = Counter(token)
-    return frequency
+    return Counter(" ".join(story).lower().split(" "))
+
 
 def main():
     '''
@@ -43,11 +37,11 @@ def main():
 
     parser = argparse.ArgumentParser(description="Retrieve the term frequency from a given story")
     parser.add_argument("--story", dest="story", required=True,  help="File name of the news article in corpus")
-
     args = parser.parse_args()
 
     story = get_story(args.story)
     count = get_tf(story)
+
     print (count)
 
 
